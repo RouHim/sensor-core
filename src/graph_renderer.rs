bef504bdbccc101e35730fcd61f22773a5d947ca
Rouven Himmelstein <rouven@himmelstein.info>
2023-08-22 08:50:19 +0200

    feat(graph): graph rendering without 3rd party plot lib

--- src/graph_renderer.rs
+++ src/graph_renderer.rs
@@ -1,13 +1,6 @@
 use std::io::{BufWriter, Cursor};
 
-use image::{ImageBuffer, Rgba};
-use imageproc::drawing;
-use poloto::build;
-use poloto::build::markers;
-use resvg::tiny_skia::{Color, Pixmap};
-use resvg::usvg::{Align, NodeExt, Options, TreeParsing};
-use resvg::{tiny_skia, usvg};
-use tagu::prelude::Elem;
+use image::{ImageBuffer, Rgba, RgbaImage};
 
 use crate::{hex_to_rgba, GraphConfig, GraphType};
 
@@ -20,22 +13,14 @@ pub fn render(graph_config: &GraphConfig) -> Vec<u8> {
     let width = graph_config.width;
     let height = graph_config.height;
 
-    // Create graph
+    // Prepare the data for the graph
     let graph_data = prepare_graph_data(width, &graph_config.sensor_values);
-    let svg_data = render_to_svg(graph_data, graph_config);
-    let graph_pixmap = render_to_raster(&svg_data, width, height, &graph_config.background_color);
 
-    // Copy pixmap to image buffer
-    let mut image = image::ImageBuffer::new(width, height);
-    for (x, y, pixel) in image.enumerate_pixels_mut() {
-        let pixel_color = graph_pixmap.pixel(x, y).unwrap();
-        *pixel = Rgba([
-            pixel_color.red(),
-            pixel_color.green(),
-            pixel_color.blue(),
-            pixel_color.alpha(),
-        ]);
-    }
+    // Render the graph
+    let mut image = match graph_config.graph_type {
+        GraphType::Line => render_line_chart(&graph_data, graph_config),
+        GraphType::LineFill => render_line_chart_filled(&graph_data, graph_config),
+    };
 
     // Draw border if border is visible
     if !graph_config.border_color.ends_with("00") {
@@ -63,7 +48,7 @@ fn draw_border(
     let border_y: i32 = 0;
     let border_width: u32 = width;
     let border_height: u32 = height;
-    drawing::draw_hollow_rect_mut(
+    imageproc::drawing::draw_hollow_rect_mut(
         image,
         imageproc::rect::Rect::at(border_x, border_y).of_size(border_width, border_height),
         border_color,
@@ -91,112 +76,128 @@ fn prepare_graph_data(width: u32, sensor_values: &Vec<f64>) -> Vec<f64> {
     plot_data
 }
 
-/// Renders the given svg data to a png image
-/// # Returns
-/// SVG data as String
-fn render_to_svg(some_numbers: Vec<f64>, config: &GraphConfig) -> String {
-    // Because we are going to extract only the path from the svg,
-    // we should render the total plot greater than the actual desired image
-    // So we get a decent quality
-    let plot_width = config.width as f64 * 2.0;
-    let plot_height = config.height as f64 * 2.0;
+/// Renders a graph based on the given config
+fn render_line_chart(numbers: &[f64], config: &GraphConfig) -> RgbaImage {
+    let width = config.width;
+    let height = config.height;
+    let min_value = config.min_sensor_value.unwrap_or(get_min(numbers)) as i32;
+    let max_value = config.max_sensor_value.unwrap_or(get_max(numbers)) as i32;
+    let line_width = config.graph_stroke_width;
+    let line_color = hex_to_rgba(&config.graph_color);
+    let background_color = hex_to_rgba(&config.background_color);
 
-    let line_data: Vec<[f64; 2]> = some_numbers
-        .iter()
-        .enumerate()
-        .map(|(i, x)| [i as f64, *x])
-        .collect();
+    let mut image = RgbaImage::from_pixel(width, height, background_color);
+    let half_line_width = (line_width / 2) as f32;
 
-    let line_plot = match config.graph_type {
-        GraphType::Line => build::plot("").line(line_data),
-        GraphType::LineFill => build::plot("").line_fill(line_data),
-    };
+    for i in 0..numbers.len() - 1 {
+        let current_value = numbers[i];
+        let next_value = numbers[i + 1];
 
-    let plot_color = &config.graph_color;
-    let plot_stroke_width = &config.graph_stroke_width;
+        // First move value between 0 and 1, where min_value is the lower bound and max_value the upper bound
+        let current_value_normalized =
+            (current_value - min_value as f64) / (max_value - min_value) as f64;
+        let next_value_normalized =
+            (next_value - min_value as f64) / (max_value - min_value) as f64;
 
-    let header = poloto::header()
-        .with_dim([plot_width, plot_height])
-        .with_viewbox([plot_width, plot_height])
-        .append(
-            poloto::render::Theme::dark()
-                .append(tagu::build::raw(format!(".poloto0.poloto_fill{{ fill: {plot_color}; }}"))) // color of first plot line filled
-                .append(tagu::build::raw(format!(".poloto_line.poloto_imgs.poloto_plot{{ stroke: {plot_color}; stroke-width: {plot_stroke_width}px; }}"))) // color of first plot line
-        );
+        // Then move the value between 0 and height
+        let img_line_start = current_value_normalized * height as f64;
+        let img_line_end = next_value_normalized * height as f64;
 
-    // Set plot x bounds
-    let mut plot_x_bounds = vec![];
-    if let Some(min_sensor_value) = config.min_sensor_value {
-        plot_x_bounds.push(min_sensor_value);
+        // Render line on image
+        let x0 = i;
+        let y0 = height - img_line_start as u32;
+
+        let x1 = i + 1;
+        let y1 = height - img_line_end as u32;
+
+        // Draw graph line
+        for offset in -half_line_width as i32..=half_line_width as i32 {
+            imageproc::drawing::draw_line_segment_mut(
+                &mut image,
+                ((x0 as f32) + offset as f32, y0 as f32),
+                ((x1 as f32) + offset as f32, y1 as f32),
+                line_color,
+            );
+        }
     }
-    if let Some(max_sensor_value) = config.max_sensor_value {
-        plot_x_bounds.push(max_sensor_value);
-    }
 
-    poloto::frame_build()
-        .data(poloto::plots!(line_plot, markers([], plot_x_bounds)))
-        .build_and_label(("", "", ""))
-        .append_to(header)
-        .render_string()
-        .unwrap()
+    image
 }
 
-/// Renders the given svg to an png image.
-/// # Returns
-/// A vector of bytes containing the RGBA8 png image
-fn render_to_raster(svg_data: &str, width: u32, height: u32, background_color: &str) -> Pixmap {
-    // Read our string into an SVG tree
-    let usvg_tree = usvg::Tree::from_str(svg_data, &Options::default()).unwrap();
+/// Renders a graph based on the given config
+fn render_line_chart_filled(numbers: &[f64], config: &GraphConfig) -> RgbaImage {
+    let width = config.width;
+    let height = config.height;
+    let min_value = config.min_sensor_value.unwrap_or(get_min(numbers)) as i32;
+    let max_value = config.max_sensor_value.unwrap_or(get_max(numbers)) as i32;
+    let line_width = config.graph_stroke_width;
+    let line_color = hex_to_rgba(&config.graph_color);
+    let fill_color = line_color;
+    let background_color = hex_to_rgba(&config.background_color);
 
-    // Extract child with id poloto_plot0
-    let plot_node = usvg_tree
-        .root
-        .children()
-        .find(|child| child.id().eq("poloto_plot0"))
-        .unwrap();
+    let mut image = RgbaImage::from_pixel(width, height, background_color);
+    let half_line_width = (line_width / 2) as f32;
 
-    let plot_path_node = plot_node.first_child().unwrap();
+    for i in 0..numbers.len() - 1 {
+        let current_value = numbers[i];
+        let next_value = numbers[i + 1];
 
-    // calculate bounding box
-    let bb_rect = plot_path_node.calculate_bbox().unwrap();
-    let bb_height = bb_rect.height();
-    let bb_width = bb_rect.width();
+        // First move value between 0 and 1, where min_value is the lower bound and max_value the upper bound
+        let current_value_normalized =
+            (current_value - min_value as f64) / (max_value - min_value) as f64;
+        let next_value_normalized =
+            (next_value - min_value as f64) / (max_value - min_value) as f64;
 
-    // Wrap into a new root
-    let new_root = usvg::Node::new(usvg::NodeKind::Group(usvg::Group::default()));
-    new_root.append(plot_path_node.clone());
+        // Then move the value between 0 and height
+        let img_line_start = current_value_normalized * height as f64;
+        let img_line_end = next_value_normalized * height as f64;
 
-    // Rendering
-    let mut resvg_tree: resvg::Tree = resvg::Tree::from_usvg_node(&new_root).unwrap();
-    resvg_tree.view_box.aspect.align = Align::XMinYMax;
+        // Render line on image
+        let x0 = i;
+        let y0 = height - img_line_start as u32;
 
-    // Fit into the desired size
-    let transform: usvg::Transform = usvg::Transform::from_translate(0f32, 0f32)
-        .post_scale(width as f32 / bb_width, height as f32 / bb_height);
+        let x1 = i + 1;
+        let y1 = height - img_line_end as u32;
 
-    // Create a new pixmap buffer to render to
-    let mut pixmap = resvg::tiny_skia::Pixmap::new(width, height)
-        .ok_or("Pixmap allocation error")
-        .unwrap();
+        // Fill the area under the line until image bottom, respect the line width
+        let mut y = y0;
+        while y < height {
+            image.put_pixel(x0 as u32, y, fill_color);
+            y += 1;
+        }
 
-    pixmap.fill(to_tiny_skia_color(background_color));
+        // Draw graph line
+        for offset in -half_line_width as i32..=half_line_width as i32 {
+            imageproc::drawing::draw_line_segment_mut(
+                &mut image,
+                ((x0 as f32) + offset as f32, y0 as f32),
+                ((x1 as f32) + offset as f32, y1 as f32),
+                line_color,
+            );
+        }
+    }
 
-    // Measure the time
-    resvg_tree.render(transform, &mut pixmap.as_mut());
-
-    // Write the pixmap buffer to a PNG file
-    pixmap
+    image
 }
 
-/// Convert a hex string to a tiny_skia color
-/// # Arguments
-/// * `hex_string` - A hex string with alpha channel like #ff0000ff
-fn to_tiny_skia_color(hex_string: &str) -> tiny_skia::Color {
-    let hex_string = hex_string.trim_start_matches('#');
-    let hex = u32::from_str_radix(hex_string, 16).unwrap();
-    let r = ((hex >> 24) & 0xff) as u8;
-    let g = ((hex >> 16) & 0xff) as u8;
-    let b = ((hex >> 8) & 0xff) as u8;
-    let a = (hex & 0xff) as u8;
-    Color::from_rgba8(r, g, b, a)
+/// Returns the minimum value of the given vector
+fn get_min(values: &[f64]) -> f64 {
+    let mut min = values[0];
+    for value in values {
+        if *value < min {
+            min = *value;
+        }
+    }
+    min
+}
+
+/// Returns the maximum value of the given vector
+fn get_max(values: &[f64]) -> f64 {
+    let mut max = values[0];
+    for value in values {
+        if *value > max {
+            max = *value;
+        }
+    }
+    max
 }
